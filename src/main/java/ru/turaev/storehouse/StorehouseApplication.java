ce129097df468a48ed04387003cea5781df8edb4
Turaev <denis.turaev@bk.ru>
2022-08-08 09:55:22 +0300

    Initial commit

---
+++ src/main/java/ru/turaev/storehouse/StorehouseApplication.java
@@ -0,0 +1,11 @@
+package ru.turaev.storehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorehouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StorehouseApplication.class, args);
+    }
+}
