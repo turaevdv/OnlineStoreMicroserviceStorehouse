937eb4a190cbd1057c15e720e7a0c2748dc277ce
Turaev <denis.turaev@bk.ru>
2022-08-09 23:11:19 +0300

    The service has become a Eureka client

--- src/main/java/ru/turaev/storehouse/StorehouseApplication.java
+++ src/main/java/ru/turaev/storehouse/StorehouseApplication.java
@@ -3,8 +3,10 @@ package ru.turaev.storehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @RefreshScope
+@EnableEurekaClient
 @SpringBootApplication
 public class StorehouseApplication {
     public static void main(String[] args) {
