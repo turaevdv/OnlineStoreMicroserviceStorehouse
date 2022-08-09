b86f3516dc547b6776bcac9562375babca8979ee
Turaev <denis.turaev@bk.ru>
2022-08-09 13:27:42 +0300

    Added Spring Boot Actuator and Spring Cloud Config

--- src/main/java/ru/turaev/storehouse/StorehouseApplication.java
+++ src/main/java/ru/turaev/storehouse/StorehouseApplication.java
@@ -2,7 +2,9 @@ package ru.turaev.storehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class StorehouseApplication {
     public static void main(String[] args) {
